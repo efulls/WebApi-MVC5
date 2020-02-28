1aec1eeb2a19dfc75ad89744bdddac15d4a86c7d
efulls <saeful.mahmud@gmail.com>
2020-02-28 16:25:13 +0700

    Testing Manual Create Web API

---
+++ WebApiProject/Controller/WelcomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+
+namespace WebApiProject.Controller
+{
+    public class WelcomeController : ApiController
+    {
+        // GET: Welcome
+        public string Get()
+        {
+            string strWelcome = "Hello World this is first my API";
+            return strWelcome;
+        }
+    }
+}
